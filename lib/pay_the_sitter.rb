27e86ca6c2f5b6e103d5fde0daa53ac889cc7759
Amy Isikoff Newell <amy.newell@gmail.com>
2012-09-05 14:52:16 -0400

    moving all the things, starting to add some tests, generally adding structure so it looks more like a real app.

---
+++ lib/pay_the_sitter.rb
@@ -0,0 +1,4 @@
+require "optparse"
+
+require_relative "pay_the_sitter/calendar_api"
+require_relative "pay_the_sitter/commandline_oauth"
---
+++ test/units/calendar_api_test.rb
@@ -0,0 +1,39 @@
+require_relative "../helper"
+require_relative "../../lib/pay_the_sitter/calendar_api"
+
+
+describe PayTheSitter::CalenderAPI do
+  # let(:page) do
+  #   posts_dir = "#{File.dirname(__FILE__)}/../fixtures/sample_app/_posts"
+  #   Jackal::Page.new("#{posts_dir}/2012-05-07-first-post.markdown")
+  # end
+
+  it "must retrieve events from the specified calendar" do
+    
+  end
+
+  it "must retrieve the events within the specified date range" do
+   
+  end
+
+  it "must convert the dates to and from the Google API format correctly" do
+   
+  end
+
+  it "must add up the number of hours of the events correctly" do
+   
+  end
+
+  it "must fail nicely if the google calendar api is not available" do
+   
+  end
+
+  it "must also fail nicely if it can't find the specified calendar, with a useful message" do
+   
+  end
+
+  it "ideally should not puke when we stay out past midnight. lulz" do
+   
+  end
+
+end
